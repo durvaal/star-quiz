cbaee655be1b451db82223fb72a2106cf91f674a
Lucas Lopes <lucas.lopesifrn@gmail.com>
2018-07-21 03:37:56 -0300

    Updated: ModalGenericComponent
    Created the modal to show the result of the quiz and other with options for the user to choice the next step.

--- src/app/components/game/shared/modal-generic/modal-generic.component.ts
+++ src/app/components/game/shared/modal-generic/modal-generic.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-generic',
@@ -7,11 +8,56 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ModalGenericComponent implements OnInit {
 
+  private form: FormGroup;
+  private results: Array<any> = [];
   @Input() personageDetail;
+  @Input() pontuation;
+  @Output() openModalNext = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
   }
 
+  ngOnInit() {
+    this.setForm();
+    this.getResults();
+  }
+
+  setForm() {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, Validators.required)
+    });
+  }
+
+  get name() {
+    return this.form.get("name").value;
+  }
+
+  getLocalStorage(key: string) {
+    JSON.parse(localStorage.getItem('key') || '[]');
+  }
+
+  setLocalStorage(key: string, value: Object) {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  saveResult() {
+    const result = {
+      name: this.form.get("name").value,
+      email: this.form.get("email").value,
+      pontuation: this.pontuation
+    }
+
+    this.results.push(result);
+    this.setLocalStorage("results", this.results);
+    this.openModalNext.emit(true);
+  }
+
+  getResults() {
+    this.results = JSON.parse(localStorage.getItem('results') || '[]');
+  }
+
+  playAgain() {
+    window.location.reload();
+  }
 }
